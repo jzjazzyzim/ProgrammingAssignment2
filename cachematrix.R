0db35de2eea4f3366c86390f5fc6b86cab3db6a1
jzjazzyzim <jzimmerman@deloitte.com>
2016-03-14 11:05:07 -0400

    Added files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These function caches the calculation of an inversion of a matrix. It is for the Coursa R Programming course.
+## The assingmnet focuses on lexical scoping
 
-## Write a short comment describing this function
+## Caches a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setmatrix <- function(inverse) m <<- inverse
+    getmatrix<- function() m
+    list(set = set, get = get,
+    setmatrix = setmatrix,
+    getmatrix = getmatrix)
 }
 
-
-## Write a short comment describing this function
+## Function to solve. Tests to see if inversion already exists
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getmatrix()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setmatrix(m)
+    m
 }
